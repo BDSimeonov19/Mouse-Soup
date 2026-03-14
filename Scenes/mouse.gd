dee1360416bf884f8428b25b0b6fa2e0e368846a
BDSimeonov19 <boris.someonov@gmail.com>
2026-03-15 00:30:07 +0200

    Fix animations. Add attack cooldown

--- Scenes/mouse.gd
+++ Scenes/mouse.gd
@@ -5,24 +5,21 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
 
-@onready var animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite := $AnimatedSprite2D
+@onready var attack_cooldown := $AttackCooldown
+
+var is_attacking := false
 
 func _process(delta: float) -> void:
-	handle_animations()
+	handle_attack_animations()
 
-func handle_animations() -> void:
-	if (!animated_sprite.is_playing()) or \
-	   (Input.is_action_just_released("left") or Input.is_action_just_released("right")):
-		animated_sprite.play("idle")
-	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
-		animated_sprite.play("walk")
-	if Input.is_action_just_pressed("attack"):
+func handle_attack_animations() -> void:
+	if Input.is_action_just_pressed("attack") and !is_attacking and attack_cooldown.is_stopped():
+		is_attacking = true
 		animated_sprite.play("attack")
-	
-	if Input.is_action_just_pressed("left"):
-		animated_sprite.flip_h = true
-	if Input.is_action_just_pressed("right"):
-		animated_sprite.flip_h = false
+		await animated_sprite.animation_finished
+		is_attacking = false
+		attack_cooldown.start()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -40,5 +37,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	handle_movement_animations(velocity)
+	
 	move_and_slide()
+
+func handle_movement_animations(velocity: Vector2) -> void:
+	if !is_attacking:
+		if velocity:
+			animated_sprite.play("walk")
+			animated_sprite.flip_h = velocity.x < 0
+		else:
+			animated_sprite.play("idle")
