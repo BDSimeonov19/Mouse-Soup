bf3a6aaae24eec94308b53bd173becf0aa228322
BDSimeonov19 <boris.someonov@gmail.com>
2026-03-22 18:14:11 +0200

    Add enemy and hit logic

---
+++ Scripts/ladybug.gd
@@ -0,0 +1,45 @@
+class_name Character extends CharacterBody2D
+
+var health = 3
+
+const SPEED = 0.0
+const JUMP_VELOCITY = -300.0
+
+@onready var ray := $RayCast2D;
+@onready var sprite := $AnimatedSprite2D;
+
+# positive means right
+var direction = 1;
+
+func _physics_process(delta: float) -> void:
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	
+	if !ray.is_colliding() and is_on_floor():
+		
+		direction *= -1;
+		scale.x *= -1;
+	
+	velocity.x = direction * SPEED
+
+	move_and_slide()
+
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area is Hitbox:
+		print("ouch")
+		take_damage();
+
+func take_damage() -> void:
+	health -= 1;
+	sprite.modulate = Color(1, 0, 0, 1)
+	await get_tree().create_timer(0.5).timeout
+	sprite.modulate = Color(1, 1, 1, 1)
+	
+	if health == 0:
+		kill();
+
+func kill() -> void:
+	queue_free()
--- Scripts/mouse.gd
+++ Scripts/mouse.gd
@@ -7,8 +7,11 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite := $AnimatedSprite2D
 @onready var attack_cooldown := $AttackCooldown
+@onready var hitbox := $Hitbox;
 
 var is_attacking := false
+
+var health := 3
 
 func _process(delta: float) -> void:
 	handle_attack_animations()
@@ -17,7 +20,9 @@ func handle_attack_animations() -> void:
 	if Input.is_action_just_pressed("attack") and !is_attacking and attack_cooldown.is_stopped():
 		is_attacking = true
 		animated_sprite.play("attack")
+		$Hitbox/CollisionShape2D.disabled = false
 		await animated_sprite.animation_finished
+		$Hitbox/CollisionShape2D.disabled = true
 		is_attacking = false
 		attack_cooldown.start()
 
@@ -46,6 +51,27 @@ func handle_movement_animations(velocity: Vector2) -> void:
 	if !is_attacking:
 		if velocity:
 			animated_sprite.play("walk")
+			flip_hitbox(velocity.x < 0)
 			animated_sprite.flip_h = velocity.x < 0
 		else:
 			animated_sprite.play("idle")
+
+func flip_hitbox(is_facing_left: bool) -> void:
+	if (is_facing_left && !animated_sprite.flip_h) || (!is_facing_left && animated_sprite.flip_h):
+		hitbox.scale.x *= -1;
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area is Hitbox:
+		take_damage()
+
+func take_damage() -> void:
+	health -= 1;
+	animated_sprite.modulate = Color(1, 0, 0, 1)
+	await get_tree().create_timer(0.5).timeout
+	animated_sprite.modulate = Color(1, 1, 1, 1)
+	if health == 0:
+		kill();
+		health = 3
+
+func kill() -> void:
+	print("you died")
