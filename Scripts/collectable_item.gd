70b54345fdb189706e7e43312bb054b8d12bc018
BDSimeonov19 <boris.someonov@gmail.com>
2026-04-30 15:03:13 +0300

    Add item pickup sound

--- Scripts/collectable_item.gd
+++ Scripts/collectable_item.gd
@@ -1,8 +1,22 @@
 class_name CollectableItem extends Node2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var sfx: AudioStreamPlayer = $SFX
+@onready var area_2d: Area2D = $AnimatedSprite2D/Area2D
+
 @export var tag : String
+@export var sprite_frames : SpriteFrames
+
+func _ready() -> void:
+	animated_sprite_2d.sprite_frames = sprite_frames
+	animated_sprite_2d.play("bob")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Character:
+		area_2d.queue_free()
+		animated_sprite_2d.queue_free()
+		sfx.play()
 		ItemsManager.item_collected(tag)
-		queue_free()
+		
+func _on_sfx_finished() -> void:
+	queue_free()
