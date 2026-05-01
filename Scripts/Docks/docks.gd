48b9573df04d2afb91ec05459576a8bc3e5cb4dc
BDSimeonov19 <boris.someonov@gmail.com>
2026-05-01 15:23:00 +0300

    Minor changes

--- Scripts/Docks/docks.gd
+++ Scripts/Docks/docks.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	
 	var collectibles = get_tree().get_nodes_in_group("collectibles")
 	for collectible in collectibles:
-		print(collectible.tag)
+		#print(collectible.tag)
 		if collectible.tag in ItemsManager.items_collected:
 			collectible.queue_free()
 
